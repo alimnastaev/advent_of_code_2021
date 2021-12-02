617479f80bc66d96418fb61b9cb63187452f631c
Alim Nastaev <a.nastaev@gmail.com>
2021-12-02 17:20:54 -0500

    day 2: part two + tests

--- day_2/day_2.ex
+++ day_2/day_2.ex
@@ -16,6 +16,32 @@ defmodule AOC_2021.Day2 do
 
   defp do_job("up " <> n, [h, d]), do: [h, d - String.to_integer(n)]
 
+  def part_two do
+    [horizontal, depth, _aim] =
+      @input
+      |> list_of_strings()
+      |> Enum.reduce([0, 0, 0], &do_job_two/2)
+
+    horizontal * depth
+  end
+
+  defp do_job_two("forward " <> n, [h, d, 0]), do: [h + String.to_integer(n), d, 0]
+
+  defp do_job_two("forward " <> n, [h, d, aim]) do
+    n = String.to_integer(n)
+    [h + n, d + n * aim, aim]
+  end
+
+  defp do_job_two("down " <> n, [h, d, aim]) do
+    n = String.to_integer(n)
+    [h, d, aim + n]
+  end
+
+  defp do_job_two("up " <> n, [h, d, aim]) do
+    n = String.to_integer(n)
+    [h, d, aim - n]
+  end
+
   defp list_of_strings(input) do
     input
     |> File.read!()
@@ -32,22 +58,27 @@ case System.argv() do
       use ExUnit.Case
 
       @expected_part_one 1_813_801
+      @expected_part_two 1_960_569_556
 
       test "tests" do
-        response = fn _ ->
+        response = fn _, _ ->
           [
-            AOC_2021.Day2.part_one()
+            AOC_2021.Day2.part_one(),
+            AOC_2021.Day2.part_two()
           ]
         end
 
-        assert [@expected_part_one] ==
-                 response.(1_813_801)
+        assert [@expected_part_one, @expected_part_two] ==
+                 response.(1_813_801, 1_960_569_556)
       end
     end
 
   ["--run"] ->
     AOC_2021.Day2.part_one()
     |> IO.inspect(label: "\npart_one \n")
+
+    AOC_2021.Day2.part_two()
+    |> IO.inspect(label: "\npart_two \n")
 
   _ ->
     IO.puts(:stderr, "\nplease specify --run or --test flag")
